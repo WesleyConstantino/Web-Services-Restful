47b1bc4621012ad7d98be66aba2b1df825931d5a
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-04 11:41:23 -0300

    Create fetch.js

---
+++ Aula 5/fetch.js
@@ -0,0 +1,21 @@
+//dá get no post de id 99
+fetch('/posts/99').
+then(resposta => resposta.json()).
+then(json => console.log(json)); 
+
+
+//cria novo post
+fetch('/posts', {
+method: 'POST',
+body: JSON.stringify({
+     title: 'Vale a pena!',
+     body: 'Este restaurante é bem legal!',
+     userId: 2
+}),
+headers: {
+   "Content-type": "application/json"
+   }
+}).
+then(resposta => resposta.json()).
+then(json => console.log(json)); 
+
